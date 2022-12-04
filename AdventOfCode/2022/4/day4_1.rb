012f7cc8d607d0a6c50080ac05fce9cb37f9d030
soubhagya <soubhagya.r.nayak@gmail.com>
2022-12-04 06:04:55 +0000

    Added solution for AoC22 day 4
    Added solution for AoC22 day 4

---
+++ AdventOfCode/2022/4/day4_1.rb
@@ -0,0 +1,10 @@
+file = File.open("day4_1.txt")
+input_data = file.readlines.map(&:chomp)
+result = 0
+for line in input_data
+    parts = line.split(/[,-]/).map(&:to_i)
+    if ((parts[0]<=parts[2]) and (parts[1]>=parts[3])) or ((parts[0]>=parts[2]) and (parts[1]<=parts[3]))
+        result += 1
+    end 
+end 
+puts result
